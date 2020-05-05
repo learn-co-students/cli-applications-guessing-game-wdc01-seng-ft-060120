54063b1f02a9d0e179cdd810bf4ca2c303629e58
Kosi Akporji <kosiazom@gmail.com>
2020-05-05 23:14:38 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
-# Code your solution here!
+def run_guessing_game
+  ran_num = rand(6)+ 1
+  input = gets.chomp
+  
+  if input.to_i == ran_num
+    puts "You guessed the correct number!"
+    
+  elsif input == "exit"
+  puts "Goodbye!"
+  
+  elsif input.to_i != ran_num
+ puts "Sorry! The computer guessed #{ran_num}."
+ 
+ #dont need to else portion because the elsif covered it and else doesnt take a condiition
+  end
+end
